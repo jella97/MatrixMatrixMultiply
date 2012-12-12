67aade8b6bd1895087069a46150e0d3e34206ffb
John <jbeal@Alicias-MacBook-Air.local>
2012-12-11 19:36:03 -0500

    initial commit

---
+++ MatrixMatrixMultiplication/AppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.m
+//  MatrixMatrixMultiplication
+//
+//  Created by John on 12/11/12.
+//  Copyright (c) 2012 IBS. All rights reserved.
+//
+
+// hello, world
+
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ MatrixMatrixMultiplication/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MatrixMatrixMultiplication
+//
+//  Created by John on 12/11/12.
+//  Copyright (c) 2012 IBS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
