0f57ce630b812398ca4b04621bca1066a22f8667
John <jbeal@Alicias-MacBook-Air.local>
2013-01-04 09:48:14 -0500

    xib file displays log info

--- MatrixMatrixMultiplication/AppDelegate.h
+++ MatrixMatrixMultiplication/AppDelegate.h
@@ -10,6 +10,22 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+// outlets
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *lblMatrixSize;
+@property (weak) IBOutlet NSTextField *lblRunTime;
+
+// properties
+@property int M;
+@property int K;
+@property int N;
+@property NSString *stringLog;
+@property NSDate *startTime;
+@property double timeInterval;
+
+// methods
+
+- (IBAction)matrixmultiply:(id)sender;
+
 
 @end
--- MatrixMatrixMultiplication/AppDelegate.m
+++ MatrixMatrixMultiplication/AppDelegate.m
@@ -56,48 +56,85 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+@synthesize M, K, N, stringLog;
+@synthesize startTime, timeInterval;
+
+- (id)init
 {
-    int i; // index for matrix
-    
-    int M = 12288; // M must be >= MAX(K, 1)  (rows in matrices A and C)                 M = lda = ldc
-	int N = M; // M >= K >= N             (columns in matrices B and C)
-	int K = M; // K must be >= MAX(N, 1)  (columns in matrix A; rows in matrix B)    K = ldb
-    
-	double alpha = 1.0;    // (Scaling factor for the product of matrices A and B.)
-    
-    double *A = calloc( (double) M * K, (double) 8 );  // (matrix A) M x K
-    for ( i = 0; i < M * K; i++ ) A[ i ] = [self getRandomDouble];
-    NSLog(@"matrix A created");
-    
-    double *B = calloc( (double) K * N, (double) 8 );  // (matrix B) K x N
-    for ( i = 0; i < K * N; i++ ) B[ i ] = [self getRandomDouble];
-    NSLog(@"matrix B created");
-    
-	double beta = 0.0;
-    double *C = calloc( (double) M * N, (double) 8 );  // (matrix C) M x N
-    NSLog(@"matrix C malloc");
-    
-	NSLog(@"start cblas_dgemm");
-	// Compute C = A B
-	cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasNoTrans,
-				 M, N, K,
-				 
-				 alpha,
-				 A, M,
-				 B, K,
-				 
-				 beta,
-				 C, M);
-	NSLog(@"end cblas_dgemm");
-    
-    NSLog(@"%f", C[0]);
-    
-    free(A);
-    free(B);
-    free(C);
+    self = [super init];
+    if ( self )
+    {
+        [self setM:4096];  // M must be >= MAX(K, 1)  (rows in matrices A and C)                M = lda = ldc
+        [self setK:4096];  // M >= K >= N             (columns in matrices B and C)
+        [self setN:4096];  // K must be >= MAX(N, 1)  (columns in matrix A; rows in matrix B)   K = ldb
+        startTime = [NSDate date];
+        timeInterval = 0;
+        [self setStringLog:@"log"];
+    }
+    return self;
 }
 
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+
+}
+
+- (IBAction)matrixmultiply:(id)sender
+{
+    dispatch_queue_t asyncQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(asyncQueue, ^{
+        
+        int i; // index for matrix
+        
+        double alpha = 1.0;    // (Scaling factor for the product of matrices A and B.)
+        
+        double *A = calloc( (double) M * K, (double) 8 );  // (matrix A) M x K
+        for ( i = 0; i < M * K; i++ ) A[ i ] = [self getRandomDouble];
+        [self setStringLog:@"matrix A created"];
+        NSLog(@"matrix A created");
+        
+        double *B = calloc( (double) K * N, (double) 8 );  // (matrix B) K x N
+        for ( i = 0; i < K * N; i++ ) B[ i ] = [self getRandomDouble];
+        [self setStringLog:[[self stringLog] stringByAppendingString:@"\nmatrix B created"]];
+        NSLog(@"matrix B created");
+        
+        double beta = 0.0;
+        double *C = calloc( (double) M * N, (double) 8 );  // (matrix C) M x N
+        [self setStringLog:[[self stringLog] stringByAppendingString:@"\nmatrix C malloc"]];
+        NSLog(@"matrix C malloc");
+        
+        [self setStringLog:[[self stringLog] stringByAppendingString:@"\nstart cblas_dgemm"]];
+
+        NSLog(@"start cblas_dgemm");
+        startTime = [NSDate date];
+
+        // Compute C = A B
+        cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                     M, N, K,
+                     
+                     alpha,
+                     A, M,
+                     B, K,
+                     
+                     beta,
+                     C, M);
+
+        [self setTimeInterval:[startTime timeIntervalSinceNow] * -1];
+
+        [self setStringLog:[[self stringLog] stringByAppendingString:@"\nend cblas_dgemm"]];
+        NSLog(@"end cblas_dgemm");
+        
+        [self setStringLog:[[self stringLog] stringByAppendingString:[NSString stringWithFormat:@"\n%f", C[0]]]];
+        NSLog(@"C[0] = %f", C[0]);
+        
+        free(A);
+        free(B);
+        free(C);
+
+        NSLog(@"timeInterval = %f", timeInterval);
+});
+
+}
 
 - (double)getRandomDouble
 {
